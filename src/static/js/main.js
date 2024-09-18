f1f8ca5cc91b59eeb5f9d6711e176dd720de7e79
deepak-linux <deepak008@live.com>
2024-09-18 07:19:21 +0530

    chore: card component width fixed

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,4 +1,3 @@
-// Powered by card 
 document.addEventListener("DOMContentLoaded", function () {
     // Select all card elements on the page
     const cards = document.querySelectorAll('.card');
@@ -7,9 +6,9 @@ document.addEventListener("DOMContentLoaded", function () {
         // Create a new element for the text
         const providerText = document.createElement('p');
         providerText.classList.add('data-provider-text');
-        providerText.innerHTML = 'Powered by SystemGuard';
+        providerText.innerHTML = 'Powered by <strong>SystemGuard</strong>';
 
-        // Append the text element to each card
+        // Append the text element to the card
         card.appendChild(providerText);
     });
 });
